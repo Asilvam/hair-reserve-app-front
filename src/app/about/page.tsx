1b489ae6daba5185a79a96e6dc542a1faec1ccdf
alejandrosilvamonsalve <alejmsilvam@falabella.cl>
2024-06-15 15:08:07 -0400

    First commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold">About Page</h1>
+        <p>Welcome to the about page.</p>
+        </div>
+    );
+}
---
+++ src/app/reservation/page.tsx
@@ -0,0 +1,8 @@
+export default function ReservationPage() {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold">Reservations Page</h1>
+            <p>Welcome to the reservations page.</p>
+        </div>
+    );
+}
